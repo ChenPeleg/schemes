17beaa2972268bbdc82e59156ad8d3d56ab7ac14
Chen Peleg <chenpeleg@gmail.com>
2025-08-04 21:57:04 +0300

    First scheme function

--- src/main.scm
+++ src/main.scm
@@ -1,17 +1,14 @@
 #lang racket
 ;; Simple Hello World program
 
-(displayln "Hello from Racket/Scheme!")
+(displayln "\nHello from Racket/Scheme! \n")
 ;; Import the factorial function from factorial.scm
-(require "factorial.scm")
+(require "simplefunctions.scm")
 
 ;; Test the function
-(displayln (format "Factorial of 5 is ~a" (factorial 5)))
+(displayln (format "5 plus 6 of 5 is ~a" (add-two-numbers 5 6)))
 
-;; Define a simple list operation
-(define my-list '(1 2 3 4 5))
-(displayln (format "My list: ~a" my-list))
-(displayln (format "Sum of list: ~a" (apply + my-list)))
+ 
 
 ;; Wait for user input before exiting
  
--- src/simplefunctions.scm
+++ src/simplefunctions.scm
@@ -1,9 +1,17 @@
 #lang racket
 
 (provide simplefunctions)
+(provide add-two-numbers)
 
 ;; Define a simple factorial function
 (define (simplefunctions n)
   (if (<= n 1)
       1
       (* n (simplefunctions (- n 1)))))
+
+(define (add-one n)
+  (+ n 1))
+
+(define (add-two-numbers a b)
+  (+ a b)
+  )
