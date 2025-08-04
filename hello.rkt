a0dc302c51f59e4c99043cb470697536b714e01b
Chen Peleg <chenpeleg@gmail.com>
2025-08-04 14:55:39 +0300

    Add initial Racket configuration and Hello World program

---
+++ hello.rkt
@@ -0,0 +1,18 @@
+#lang racket
+;; Simple Hello World program
+
+(displayln "Hello from Racket/Scheme!")
+
+;; Define a simple function
+(define (factorial n)
+  (if (<= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+;; Test the function
+(displayln (format "Factorial of 5 is ~a" (factorial 5)))
+
+;; Define a simple list operation
+(define my-list '(1 2 3 4 5))
+(displayln (format "My list: ~a" my-list))
+(displayln (format "Sum of list: ~a" (apply + my-list)))
